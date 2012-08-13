2d223c89442d79b0492d383759596b7d33ca45b9
Marc O'Morain <github.com@marcomorain.com>
2012-08-13 23:52:14 +0100

    add entity extraction to the build

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-make bsp2json && ./bsp2json output/maps/$1.bsp && cp output/maps/*.json public
+make bsp2json && ./bsp2json output/maps/$1.bsp && \
+cp output/maps/*.vertices.json public && \
+cp output/maps/*.indices.json  public && \
+python entities2json.py output/maps/$1.bsp.entities.json > public/$1.bsp.entities.json
